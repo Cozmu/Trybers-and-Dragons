bbd3a5c3a23bdaf7c897a966d41e481f90899cdb
Cozmu <www.jorgereis@gmail.com>
2023-03-27 17:56:11 -0300

    feat: add class character

--- src/Character.ts
+++ src/Character.ts
@@ -1,18 +1,10 @@
-import Archetype from './Archetypes';
+import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
 import Fighter from './Fighter';
-import Race from './Races';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
 
-type CharacterParams = {
-  race:Race,
-  archetype:Archetype,
-  maxLifePoints:number,
-  lifePoints:number,
-  strength:number,
-  defense:number,
-  dexterity:number,
-  energy:Energy,
-};
+const Newdexterity = getRandomInt(1, 10);
 
 class Character implements Fighter {
   private _race:Race;
@@ -21,22 +13,89 @@ class Character implements Fighter {
   private _lifePoints:number;
   private _strength:number;
   private _defense:number;
-  private _dexterity:number;
   private _energy:Energy;
-  constructor(params: CharacterParams) {
-    this._race = params.race;
-    this._archetype = params.archetype;
-    this._maxLifePoints = params.maxLifePoints;
-    this._lifePoints = params.lifePoints;
-    this._strength = params.strength;
-    this._defense = params.defense;
-    this._dexterity = params.dexterity;
-    this._energy = params.energy;
+  private _dexterity:number;
+
+  constructor(
+    private _name:string,
+  ) {
+    this._race = new Elf(_name, Newdexterity);
+    this._dexterity = Newdexterity;
+    this._archetype = new Mage(_name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = { 
+      type_: this._archetype.energyType, 
+      amount: getRandomInt(1, 10), 
+    };
   }
 
-  // set dexterity(param: number) {
-  //   this. = param;
+  receiveDamage(attackPoints: number):number {
+    const damange = attackPoints - this._defense;
+    if (damange > 0) {
+      this._lifePoints -= damange;
+    }
+    if (damange < 0) {
+      this._lifePoints -= 1;
+    }
+    if (this.lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this.lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+  
+  levelUp(): void {
+    const newmaxLifePoints = getRandomInt(1, 10) + this._maxLifePoints; 
+    if (newmaxLifePoints < this._race.maxLifePoints) {
+      this._maxLifePoints = newmaxLifePoints;
+      this._lifePoints = this._maxLifePoints;
+    } else {
+      this._maxLifePoints = this._race.maxLifePoints;
+      this._lifePoints = this._maxLifePoints;
+    }
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+  }
+
+  // special(enemy: Fighter): void {
+  // IMPLEMENTAR (DANO EM RELAÇÃO A CLASSE)
   // }
+
+  get race():Race {
+    return this._race;
+  }
+
+  get archetype():Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints():number {
+    return this._lifePoints;
+  }
+
+  get strength():number {
+    return this._strength;
+  }
+
+  get defense():number {
+    return this._defense;  
+  }
+
+  get dexterity():number {
+    return this._dexterity;
+  }
+
+  get energy():Energy {
+    return { ...this._energy };
+  }
 }
 
 export default Character;
